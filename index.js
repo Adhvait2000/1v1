9572978fcbd24ac1c2a7dbe4870a199e905a0e38
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-06-18 23:09:56 +0800

    Game Timer and Game Over

--- index.js
+++ index.js
@@ -135,6 +135,43 @@ function rectangularCollision({rectangle1,rectangle2}){
         rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
     )
 }
+
+//If one of the player's health gets completely depleted before time runs out
+function determineWinner({player, enemy, timerId})
+{
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if (player.health > enemy.health)
+    {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    }
+    else if (player.health < enemy.health)
+    {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    }
+}
+
+//decreasing the time on the timer
+let timer = 60
+let timerId 
+function decreaseTimer(){
+    if(timer > 0) 
+    {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if(timer == 0)
+    {
+        determineWinner({player, enemy, timerId})
+    }
+}
+
+decreaseTimer()
+
+
 // creating animation loop so that the player and enemy stick to the bottom of the canvas and "gravity" is a factor
 function animate(){
     window.requestAnimationFrame(animate)
@@ -175,6 +212,12 @@ function animate(){
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0)
+    {
+        determineWinner({player, enemy, timerId})
     }
 }
 
