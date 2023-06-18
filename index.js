ae971d8c3b2af1866be5c33daf1bd061ac160061
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-06-18 20:55:43 +0800

    Making the Player and Enemy move

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ class Sprite{
         this.position = position;
         this.velocity = velocity;
         this.height = 150;
+        this.lastKey 
     }
 
     draw(){
@@ -27,6 +28,7 @@ class Sprite{
     //properties that are updated to move the Sprite around
     update(){
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         //to ensure that the rectangle stops at the bottom of the canvas
@@ -63,6 +65,22 @@ const enemy = new Sprite({
     }
 })
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    }
+}
+
+let lastKey
 // creating animation loop so that the player and enemy stick to the bottom of the canvas and "gravity" is a factor
 function animate(){
     window.requestAnimationFrame(animate)
@@ -70,6 +88,78 @@ function animate(){
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+
+    //Player movement
+    if (keys.a.pressed && lastKey == 'a'){
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey == 'd') {
+        player.velocity.x = 1
+    }
+
+    //Enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey == 'ArrowLeft'){
+        enemy.velocity.x = -1
+    } else if (keys.ArrowRight.pressed && enemy.lastKey == 'ArrowRight') {
+        enemy.velocity.x = 1
+    }
 }
 
 animate()
+
+//adding an event listener
+//moving the player to the right left and up
+window.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    switch (event.key){
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -10
+            break
+
+        //For the enemy
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -10
+            break
+    }
+    console.log(event.key);
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key){
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+       
+    }
+    //enemy keys
+    switch (event.key){
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+    }
+    
+})
