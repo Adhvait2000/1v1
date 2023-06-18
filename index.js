8ec4159f92ca7cbdbe0f72cb54db4362c833b640
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-06-18 20:57:27 +0800

    Increasing speed of movement

--- index.js
+++ index.js
@@ -93,16 +93,16 @@ function animate(){
 
     //Player movement
     if (keys.a.pressed && player.lastKey == 'a'){
-        player.velocity.x = -1
+        player.velocity.x = -5
     } else if (keys.d.pressed && player.lastKey == 'd') {
-        player.velocity.x = 1
+        player.velocity.x = 5
     }
 
     //Enemy movement
     if (keys.ArrowLeft.pressed && enemy.lastKey == 'ArrowLeft'){
-        enemy.velocity.x = -1
+        enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey == 'ArrowRight') {
-        enemy.velocity.x = 1
+        enemy.velocity.x = 5
     }
 }
 
